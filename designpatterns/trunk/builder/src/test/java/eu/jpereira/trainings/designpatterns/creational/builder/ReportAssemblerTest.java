70ed79229bd218709eefcd15bff20dcb20d059ce
Bres1990 <adam.poteralowicz@gmail.com>
2014-11-26 05:19:21 +0100

    Builder exercises.

--- designpatterns/trunk/builder/src/test/java/eu/jpereira/trainings/designpatterns/creational/builder/ReportAssemblerTest.java
+++ designpatterns/trunk/builder/src/test/java/eu/jpereira/trainings/designpatterns/creational/builder/ReportAssemblerTest.java
@@ -19,24 +19,41 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import eu.jpereira.trainings.designpatterns.creational.builder.json.JSONRaportBuilder;
 import eu.jpereira.trainings.designpatterns.creational.builder.model.Customer;
+import eu.jpereira.trainings.designpatterns.creational.builder.model.RaportBuilder;
 import eu.jpereira.trainings.designpatterns.creational.builder.model.Report;
 import eu.jpereira.trainings.designpatterns.creational.builder.model.SaleEntry;
 import eu.jpereira.trainings.designpatterns.creational.builder.model.SoldItem;
+import eu.jpereira.trainings.designpatterns.creational.builder.xml.XMLReportBuilder;
 
 /**
  * @author jpereira
  * 
  */
 public class ReportAssemblerTest {
+	@Test
+	public void testWyowalaniaReportBody() {
+
+		RaportBuilder raportBuilder = new JSONRaportBuilder();
+		ReportAssembler assembler = new ReportAssembler();
+		assembler.setSaleEntry(createDummySaleEntry());
+		Report report = assembler.getReport(raportBuilder);
+		
+		String expected = "sale:{customer:{name:\"Bob\",phone:\"1232232\"},items:[{name:\"Computer\",quantity:2,price:99.9},{name:\"Printer\",quantity:1,price:79.8}]}";
+		assertEquals(expected, report.getAsString());
+	}
 
 	@Test
 	public void testAssembleJSONReportBody() {
 
 		ReportAssembler assembler = new ReportAssembler();
-
 		assembler.setSaleEntry(createDummySaleEntry());
-		Report report = assembler.getReport("JSON");
+
+		// Report report = assembler.getReport("JSON");
+		RaportBuilder raportBuilder = new JSONRaportBuilder();
+		Report report = assembler.getReport(raportBuilder);
+
 		String expected = "sale:{customer:{name:\"Bob\",phone:\"1232232\"},items:[{name:\"Computer\",quantity:2,price:99.9},{name:\"Printer\",quantity:1,price:79.8}]}";
 		assertEquals(expected, report.getAsString());
 	}
@@ -45,11 +62,13 @@ public class ReportAssemblerTest {
 	public void testAssembleXMLReportBody() {
 
 		ReportAssembler assembler = new ReportAssembler();
-
 		assembler.setSaleEntry(createDummySaleEntry());
-		Report report = assembler.getReport("XML");
-		String expected = "<sale><customer><name>Bob</name><phone>1232232</phone></customer><items><item><name>Computer</name><quantity>2</quantity><price>99.9</price></item><item><name>Printer</name><quantity>1</quantity><price>79.8</price></item></items></sale>";
 
+		// Report report = assembler.getReport("XML");
+		RaportBuilder raportBuilder = new XMLReportBuilder();
+		Report report = assembler.getReport(raportBuilder);
+
+		String expected = "<sale><customer><name>Bob</name><phone>1232232</phone></customer><items><item><name>Computer</name><quantity>2</quantity><price>99.9</price></item><item><name>Printer</name><quantity>1</quantity><price>79.8</price></item></items></sale>";
 		assertEquals(expected, report.getAsString());
 
 	}
@@ -58,9 +77,12 @@ public class ReportAssemblerTest {
 	public void testAssembleHTMLReportBody() {
 
 		ReportAssembler assembler = new ReportAssembler();
-
 		assembler.setSaleEntry(createDummySaleEntry());
-		Report report = assembler.getReport("HTML");
+
+		// Report report = assembler.getReport("HTML");
+		RaportBuilder raportBuilder = new HTMLReportBuilder();
+		Report report = assembler.getReport(raportBuilder);
+
 		String expected = "<span class=\"customerName\">Bob</span><span class=\"customerPhone\">1232232</span><items><item><name>Computer</name><quantity>2</quantity><price>99.9</price></item><item><name>Printer</name><quantity>1</quantity><price>79.8</price></item></items>";
 
 		assertEquals(expected, report.getAsString());
